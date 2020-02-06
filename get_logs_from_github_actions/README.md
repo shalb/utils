28e370e46b4823422f34795605ba05fcb53d3cba
MaxymVlasov <MaxymVlasov@users.noreply.github.com>
2020-02-06 20:49:52 +0200

    Move configuration separately, remove hardcore, improve logging

--- get_logs_from_github_actions/README.md
+++ get_logs_from_github_actions/README.md
@@ -4,5 +4,5 @@
 
 * Admin rights to repo
 * `GITHUB_TOKEN` with access to repo. [How create](https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line#creating-a-token)
-* `export GITHUB_TOKEN` to env
+* copy `configuration.py.example` to `configuration.py` and set what you need
 * python 3.6+
--- get_logs_from_github_actions/main.py
+++ get_logs_from_github_actions/main.py
@@ -1,28 +1,37 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-import os
 import requests
 import json
 import time
+import configuration as cfg
 
-TOKEN=os.environ['GITHUB_TOKEN']
-ORG='shalb'
-REPO='cluster.dev'
-WORKFLOW_FILENAME='gh-5.yaml'
 
-headers = {'Authorization': f'token {TOKEN}'}
+headers = {'Authorization': f'token {cfg.github_token}'}
+
+def sleep_logs(seconds, msg):
+    color = '\033[90m'
+    reset_color = '\033[0m'
+
+    if cfg.enable_sleep_logs:
+        print(f'{color}{msg}, sleep {seconds}s{reset_color}')
+
+    time.sleep(seconds)
 
 
 def get_run_id(org, repo, workflow, headers):
     url = f'https://api.github.com/repos/{org}/{repo}/actions/workflows/{workflow}/runs'
     r = requests.get(url, headers=headers)
-    run_id = json.loads(r.text)['workflow_runs'][0]['id']
+    try:
+        run_id = json.loads(r.text)['workflow_runs'][0]['id']
+    except KeyError:
+        print('\033[93mCheck that "workflow_filename" variable set right\033[0m')
+        exit(0)
 
     return run_id
 
 
 def get_last_job_id(org, repo, run_id, headers):
-    url = f'https://api.github.com/repos/{ORG}/{REPO}/actions/runs/{run_id}/jobs'
+    url = f'https://api.github.com/repos/{org}/{repo}/actions/runs/{run_id}/jobs'
     r = requests.get(url, headers=headers)
     job_id = json.loads(r.text)['jobs'][0]['id']
 
@@ -35,7 +44,6 @@ def get_logs(org, repo, job_id, headers):
     """
     old_logs = []
     no_logs = 0 # iterations
-    second_sleep = 3
 
     while True:
         url = f'https://api.github.com/repos/{org}/{repo}/actions/jobs/{job_id}/logs'
@@ -48,26 +56,33 @@ def get_logs(org, repo, job_id, headers):
             print('\n'.join(difference))
             no_logs = 0
         else:
-            no_logs += 1 # iterations
+            no_logs += 1
 
-        if no_logs > 10: # iterations
+        if no_logs == cfg.leave_job_after_no_logs_iterations:
             break
 
         old_logs = logs
-        print (f'\033[90mNo new logs in job {job_id}, waiting {second_sleep}s\033[0m')
-        time.sleep(second_sleep) # seconds
+        sleep_logs(cfg.job_logs_update_interval, f'No new logs in job {job_id}')
 
 
-old_job_id = 0
+def main():
+    old_job_id = 0
 
-run_id = get_run_id(ORG, REPO, WORKFLOW_FILENAME, headers)
-job_id = get_last_job_id(ORG, REPO, run_id, headers)
+    run_id = get_run_id(cfg.org, cfg.repo, cfg.workflow_filename, headers)
+    job_id = get_last_job_id(cfg.org, cfg.repo, run_id, headers)
 
-while True:
-    if job_id != old_job_id:
-        get_logs(ORG, REPO, job_id, headers)
+    while True:
+        if job_id != old_job_id:
+            print('\n'*80, f'Get logs from: https://github.com/{cfg.org}/{cfg.repo}/runs/{job_id}')
+            get_logs(cfg.org, cfg.repo, job_id, headers)
 
-    print (f'\033[90mNo new jobs in workflow {WORKFLOW_FILENAME}, sleep 10s\033[0m')
-    time.sleep(10)
+        sleep_logs(cfg.search_for_new_jobs_interval, f'No new jobs in workflow {workflow_filename}')
 
-    old_job_id = job_id
+        old_job_id = job_id
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except (KeyboardInterrupt, SystemExit):
+        exit(0)
